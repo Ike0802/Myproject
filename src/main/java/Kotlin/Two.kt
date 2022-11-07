4ccda7a3cf9e8570ecb68ed1b7f1aabec096482f
Ike Lin <lina880063@gmail.com>
2022-11-07 21:37:20 +0800

    Initial commit

---
+++ src/main/java/Kotlin/Two.kt
@@ -0,0 +1,15 @@
+package Kotlin
+
+
+fun main (args : Array <String>){
+//    println("Helllo Kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+       println("Hello Kotlin")
+    }
+}
---
+++ src/main/java/New/Person.java
@@ -0,0 +1,7 @@
+package New;
+
+public class Person {
+    public void hello(){
+        System.out.println("Hello World");
+    }
+}
